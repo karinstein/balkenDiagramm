47b9ffc2abee96a73efb0a541d8ace6c9a7c5972
Karina Arinstein <etvidi@gmail.com>
2021-03-01 15:27:05 +0100

    intermidiate chaos

--- js/index.js
+++ js/index.js
@@ -51,54 +51,102 @@ document.addEventListener("DOMContentLoaded", evt => {
       }
     }
 
-    const zeichneDiagramm = array => {
+    // let val = k*i + b;
+    // ctx.fillRect(val, h-30, 10,-20*elem);
+
+    const zeichneEinfachesDiagramm = array => {
         ctx.clearRect(0, 0, w, h);
-        ctx.fillStyle = 'green';
-        ctx.fillRect(10, 10, 10, h);
-        // ctx.stroke();
-        // if (fillStyle) {
-        //     ctx.fillStyle = fillStyle;
-        //     ctx.fill();
-        // }
         let arrayNorm = findeMinMax(array);
         let k = arrayNorm.k, b = arrayNorm.b;
-        array.forEach((elem, ind) => {
-            ctx.fillStyle = 'green';
+        let minVal = arrayNorm.minVal, maxVal = arrayNorm.maxVal;
+        console.log(arrayNorm);
+        ctx.fillStyle = 'green';
+        array.forEach((elem, i) => {
+            // einfaches Zeichen
             let val = k*elem + b;
-            ctx.fillRect(val, 10, 10, h);
-            console.log(elem,val);
+            ctx.fillRect(val, h-30, 10, -h);
+            // console.log(elem,val);
         });
-        // ctx.beginPath();
-        // ctx.moveTo(20, 20);
-        // ctx.lineTo(20, 100);
-        // ctx.lineTo(70, 100);
-        // ctx.strokeStyle = "red";
-        // ctx.stroke();
+        ctx.font = "20px Arial";
+        ctx.fillStyle = 'black';
+        ctx.fillText(`${minVal}`, 0, h);
+        let txt = `${maxVal}`;
+        ctx.fillText(txt, w-ctx.measureText(txt).width, h);
+    }
+
+    const zeichneDiagramm = binnedArray => {
+        let dataMap = binnedArray.binnedMap;
+        for (const [key, value] of Object.entries(dataMap)) {
+            console.log(`${key}: ${value}`);
+        }
+    }
+
+    const bereiteDatenVor = (array,nBins,xMin,xMax) => {
+        // if not specified, use the whole data range to find min and max values
+        if (!xMin || !xMax) {
+           let minMax = findeMinMax(array);
+           xMin = minMax.minVal;
+           xMax = minMax.maxVal+1;
+        }
+        console.log(nBins,xMin,xMax);
+
+        // initialize binned data array
+        // versuche später mit Map
+        let binnedArrayX = new Array(nBins);
+        let binnedArrayY = new Array(nBins);
+        let binnedMap = {};
+        for (let i=0; i<nBins; i++){
+          binnedArrayY[i] = 0;
+        }
+
+        // fill in binned data array
+        let dx = (xMax - xMin)/nBins;
+        array.forEach(xi => {
+            let i = ~~((xi-xMin)/dx);
+            binnedArrayY[i]++;
+        });
+        for (let i=0; i<nBins; i++){
+            let xi0 = xMin + i*dx;
+            console.log(i,xi0);
+            binnedMap[xi0] = binnedArrayY[i];
+            binnedArrayX[i] = xi0;
+        }
+        return {
+          binnedArrayX: binnedArrayX,
+          binnedArrayY: binnedArrayY,
+          binnedMap: binnedMap,
+          nBins: nBins,
+          xMin: xMin,
+          xMax: xMax,
+          dx: dx
+        };
     }
 
     const manageData = json => {
         // let arr = Array.from(json).slice(0,10);
         let arr = json.slice(0,50);
-        console.log(arr);
-        zeichneDiagramm(arr);
+        // console.log(arr);
+        let binnedArray = bereiteDatenVor(arr,20);
+        console.log(binnedArray);
+        zeichneEinfachesDiagramm(arr);
+        zeichneDiagramm(binnedArray);
     }
 
 
     const readJson = () => {
-        // fetch('./data/gerichte.json')
         fetch('./data/data.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error("HTTP error " + response.status);
             }
-            console.log(response);
+            // console.log(response);
             return response.json();
         })
         .then(json => {
             manageData(json);
         })
         .catch(function () {
-            console.log('Some fetch error...');;
+            console.log('Some unknown error...');;
         })
     }
 
@@ -108,7 +156,6 @@ document.addEventListener("DOMContentLoaded", evt => {
     }
 
     // EVENT-LISTENERS
-    // myCanvas.addEventListener('click', naechsterZug);
 
     // INIT
     init();
